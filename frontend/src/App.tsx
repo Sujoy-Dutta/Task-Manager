4ef836b544ed4e5488ac06f42d53abda9c072dd6
unknown <dsujoy387@gmail.com>
2026-05-05 20:47:12 +0530

    Created React project and updated index file.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { TaskProvider } from './contexts/TaskContext';
+import PrivateRoute from './components/PrivateRoute';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import DashboardPage from './pages/DashboardPage';
+import './App.css';
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/login"  element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+
+      <Route element={<PrivateRoute />}>
+        <Route
+          path="/dashboard"
+          element={
+            <TaskProvider>
+              <DashboardPage />
+            </TaskProvider>
+          }
+        />
+      </Route>
+
+      {/* Catch-all: redirect to dashboard (PrivateRoute will redirect to login if not authed) */}
+      <Route path="*" element={<Navigate to="/dashboard" replace />} />
+    </Routes>
+  );
+}
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
+  );
+}
